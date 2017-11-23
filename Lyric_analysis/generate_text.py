873c4b9c470e56492455fc971e3cacb4575824d8
silburt <silburt@astro.utoronto.ca>
2017-11-23 11:06:17 -0500

    edit

--- Lyric_analysis/generate_text.py
+++ Lyric_analysis/generate_text.py
@@ -1,22 +1,21 @@
 import numpy as np
-import glob
 import sys
 from keras.models import load_model
-from clean_lyrics import *
 
 # text generation
 def gen(genre,dir_model,seq_length):
     dir_lyrics = 'playlists/%s/'%genre
-    X = np.load('%s/X_sl%d.npy'%(dir_lyrics,seq_length))
-    char_to_int = np.load('%schar_to_int.npy'%dir_lyrics)
-    int_to_char = np.load('%sint_to_char.npy'%dir_lyrics)
+    
+    model = load_model(dir_model)
+    X = np.load('%s/X_sl%d_2.npy'%(dir_lyrics,seq_length))
+    char_to_int, int_to_char, n_chars = np.load('%sancilary_2.npy'%dir_lyrics)
     
     #seed = np.random.randint(0, n_songs)
     #ini = songs[seed][0:seq_length]     #set initial = start of a song
     ini = X[0]
-    pattern = [char_to_int[v] for v in list(ini)]
+    pattern = [int(v*n_chars) for v in list(ini)]
 #    print("Song:%s"%files[seed])
-    print("Seed:%s"%ini)
+#    print("Seed:%s"%ini)
     # generate characters
     for i in range(100):
         x = np.reshape(pattern, (1, len(pattern), 1))
@@ -25,7 +24,7 @@ def gen(genre,dir_model,seq_length):
         #index = np.argmax(pred)
         index = np.random.choice(len(pred[0]), p=pred[0])
         result = int_to_char[index]
-        seq_in = [int_to_char[value] for value in pattern]
+        #seq_in = [int_to_char[value] for value in pattern]
         sys.stdout.write(result)
         pattern.append(index)
         pattern = pattern[1:len(pattern)]
