cea13a8708a67b09c9853b305ae087cb71510a04
silburt <silburt@astro.utoronto.ca>
2017-11-25 16:45:27 -0500

    edit

--- Lyric_analysis/generate_text.py
+++ Lyric_analysis/generate_text.py
@@ -7,8 +7,8 @@ def gen(genre,dir_model,seq_length):
     dir_lyrics = 'playlists/%s/'%genre
     
     model = load_model(dir_model)
-    X = np.load('%s/X_sl%d_2.npy'%(dir_lyrics,seq_length))
-    char_to_int, int_to_char, n_chars = np.load('%sancillary_2.npy'%dir_lyrics)
+    X = np.load('%s/X_sl%d.npy'%(dir_lyrics,seq_length))
+    char_to_int, int_to_char, n_chars = np.load('%sancillary.npy'%dir_lyrics)
     
     #seed = np.random.randint(0, n_songs)
     #ini = songs[seed][0:seq_length]     #set initial = start of a song
