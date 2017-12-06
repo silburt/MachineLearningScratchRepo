e9781cc07d02201773b7f0f5261dab2317b6aff7
ARI JOSHUA SILBURT <ajs725@aci-lgn-001.acib.production.int.aci.ics.psu.edu>
2017-12-06 13:14:10 -0500

    edit

--- SparkBigData/BigData/Clustering/clustering.py
+++ SparkBigData/BigData/Clustering/clustering.py
@@ -1,15 +1,21 @@
+'''
+You can execute this script (in Python 3) by doing:
+pyspark
+exec(open("clustering.py").read())
+'''
+
 rdd1 = sc.textFile("5000_points.txt")
 
 rdd2 = rdd1.map(lambda x:x.split())
 rdd3 = rdd2.map(lambda x: [int(x[0]),int(x[1])])
 
-rdd3.persist(StorageLevel.MEMORY_ONLY)
+#rdd3.persist(StorageLevel.MEMORY_ONLY)
 
 from pyspark.mllib.clustering import KMeans
 
 for clusters in range(1,30):
     model = KMeans.train(rdd3, clusters)
-    print clusters, model.computeCost(rdd3)
+    print(clusters, model.computeCost(rdd3))
 
 for trials in range(10):                          #Try ten times to find best result
     for clusters in range(12, 16):                 #Only look in interesting range
@@ -17,7 +23,7 @@ for trials in range(10):                          #Try ten times to find best re
         cost = model.computeCost(rdd3)
         centers = model.clusterCenters             #Let's grab cluster centers
         if cost<1e+13:                             #If result is good, print it out
-            print clusters, cost
+            print(clusters, cost)
             for coords in centers:
-                print int(coords[0]), int(coords[1])
+                print(int(coords[0]), int(coords[1]))
             break
