195d6ad4c7a89ff68e437f58e9400c6d5549e840
silburt <silburt@astro.utoronto.ca>
2017-12-11 15:09:46 -0500

    train word lstm

--- Lyric_analysis/train_lstm.py
+++ Lyric_analysis/train_lstm.py
@@ -14,7 +14,7 @@ import sys
 
 # https://blog.keras.io/using-pre-trained-word-embeddings-in-a-keras-model.html
 def get_embedding_matrix(text_to_int,embed_dim,y):
-    f = open('utils/glove.6B/glove.6B.%dd.txt'%embed_dim,'r')
+    f = open('utils/glove.6B/glove.6B.%dd.txt'%embed_dim,'r',encoding='utf-8')
     
     embeddings_index = {}
     for line in f:
@@ -32,23 +32,25 @@ def get_embedding_matrix(text_to_int,embed_dim,y):
             # words not found in embedding index will be all-zeros.
             embedding_matrix[i] = embedding_vector
 
-    # convert y-data to embed
-    for 
+    # embed y-data
+    y_embed = np.zeros((len(y), embed_dim))
+    for i in range(len(y)):
+        y_embed[i] = embedding_matrix[y[i]]
 
-    return embedding_matrix
+    return embedding_matrix, y_embed
 
 def train_model(genre,dir_model,seq_length,epochs,batch_size,word_or_character,embed_dim=50):
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=True)) #check gpu is being used
     
+    text_to_int, int_to_text, n_chars = np.load('playlists/%s/ancillary_%s.npy'%(genre,word_or_character))
     X = np.load('playlists/%s/X_sl%d_%s.npy'%(genre,seq_length,word_or_character))
     y = np.load('playlists/%s/y_sl%d_%s.npy'%(genre,seq_length,word_or_character))
-    
     if word_or_character == 'word':
         embedding_matrix, y = get_embedding_matrix(text_to_int,embed_dim,y)
+        print('Successfully generated word embeddings')
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    text_to_int, int_to_text, n_chars = np.load('%sancillary_%s.npy'%%(dir_lyrics,word_or_character))
-    
+
     try:
         model = load_model(dir_model)
         print("successfully loaded previous model, continuing to train")
@@ -56,25 +58,22 @@ def train_model(genre,dir_model,seq_length,epochs,batch_size,word_or_character,e
         print("generating new model")
         model = Sequential()
         
-        nb_classes = len(char_to_int)
+        #nb_classes = len(char_to_int)
         #input_shape, output_shape = (seq_length,), (input_shape[0], nb_classes)
         #input = Input(shape=input_shape, dtype='uint8')
         #model.add(Lambda(K.one_hot,arguments={'nb_classes': nb_classes}, output_shape=output_shape)(input))
         
         # old network
-#        model.add(LSTM(512, dropout=0.2, recurrent_dropout=0.2, return_sequences=True, input_shape=(X.shape[1], X.shape[2])))
-#        model.add(LSTM(512, dropout=0.2, recurrent_dropout=0.2, return_sequences=False))
-#        model.add(Dense(y.shape[1], activation='softmax'))
 #        model.add(GRU(512, dropout=0.2, recurrent_dropout=0.2, return_sequences=True, input_shape=(X.shape[1], X.shape[2])))
 #        model.add(GRU(512, dropout=0.2, recurrent_dropout=0.2, return_sequences=False))
 #        model.add(Dense(y.shape[1], activation='softmax'))
 
-        #embedding- so probably the thing to do is use a frozen weight pre-trained embedding layer (word2vec), and the output is an embedding vector. I.e. convert all your words to embedding.
+        # frozen weight embedding
         model.add(Embedding(len(text_to_int), embed_dim, weights=[embedding_matrix],
                             input_length=seq_length, trainable=False))
         model.add(GRU(embed_dim, dropout=0.2, recurrent_dropout=0.2, return_sequences=True))
         model.add(GRU(512, dropout=0.2, recurrent_dropout=0.2, return_sequences=False))
-        model.add(Dense(len(text_to_int)))
+        model.add(Dense(embed_dim))
 
         optimizer = Adam(lr=2e-5, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
         model.compile(loss='categorical_crossentropy', optimizer=optimizer)
@@ -89,12 +88,11 @@ def train_model(genre,dir_model,seq_length,epochs,batch_size,word_or_character,e
 if __name__ == '__main__':
     genre = 'country'
     word_or_character = 'word'
-    #seq_length = 200
     seq_length = int(sys.argv[1])
-    dir_model = 'models/%s_sl%d_%s.h5'%(genre,seq_length,word_or_character)
-    
     epochs = 40
     batch_size = 256
+    
+    dir_model = 'models/%s_sl%d_%s.h5'%(genre,seq_length,word_or_character)
     
     train_model(genre,dir_model,seq_length,epochs,batch_size,word_or_character)
 
