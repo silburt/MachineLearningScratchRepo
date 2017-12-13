cef8b26745b62996b96a16093c59bc187b940ddf
silburt <silburt@astro.utoronto.ca>
2017-12-13 10:51:47 -0500

    edit

--- Lyric_analysis/check_data.py
+++ Lyric_analysis/check_data.py
@@ -6,14 +6,16 @@ N_check = 50
 embed_dim = 50
 seq_length = [4]#,6,8,10,15]
 
-text_to_int, int_to_text, n_chars = np.load('playlists/%s/ancillary_%s.npy'%(genre,word_or_character))
+dir = 'playlists/%s/'%genre
+text_to_int, int_to_text, n_chars = np.load('%sancillary_%s.npy'%(dir,word_or_character))
 if word_or_character == 'word':
-    embedding_matrix = np.load('playlists/%s/embedding_matrix_%dd.npy'%(genre,embed_dim))
+    embedding_matrix = np.load('%sembedding_matrix_%dd.npy'%(dir,embed_dim))
     labels = list(text_to_int.keys())
 
 for sl in seq_length:
-    X = np.load('playlists/%s/X_sl%d_%s.npy'%(genre,sl,word_or_character))
-    y = np.load('playlists/%s/y_sl%d_%s.npy'%(genre,sl,word_or_character))
+    X = np.load('%sX_sl%d_%s.npy'%(dir,sl,word_or_character))
+    y = np.load('%sy_sl%d_%s.npy'%(dir,sl,word_or_character))
+    song_names = np.load('%ssong_names_sl%d_%s.npy'%(dir,sl,word_or_character))
 
     ran = np.random.randint(0,X.shape[0],N_check)
     for r in ran:
@@ -24,4 +26,4 @@ for sl in seq_length:
             why = labels[np.argmax(np.sum(y[r]*embedding_matrix,axis=1))]
         elif word_or_character == 'character':
             why = int_to_text[y[r]]
-        print(ex, why)
+        print(ex, why, song_names[r])
