95268196bc7e28c4f40cd0036547c469e9046f37
silburt <silburt@astro.utoronto.ca>
2018-01-15 16:57:57 -0500

    one-hot on the fly

--- Lyric_analysis/train_lstm_char.py
+++ Lyric_analysis/train_lstm_char.py
@@ -11,7 +11,6 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, GRU, Embedding, TimeDistributed
-#from keras.layers import Lambda, Input
 from keras.optimizers import Adam, RMSprop
 from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
@@ -20,10 +19,21 @@ from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import sys
 
-def train_model(genre, dir_model, seq_length, batch_size, epochs=100):
+# one-hot encode on the fly, saves tons of memory
+def one_hot_gen(X, Y, vocab_size, seq_length, batch_size=64):
+    while True:
+        for i in range(0, len(X), batch_size):
+            x, y = X[i:i+batch_size].copy(), Y[i:i+batch_size].copy()
+            x = np.eye(vocab_size)[x]
+            y = np.eye(vocab_size)[y]
+            yield (x, y)
+
+# main routine
+def train_model(genre, dir_model, seq_length, batch_size, epochs=100, lstm_size=256):
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=True)) #check gpu is being used
     
     text_to_int, int_to_text, n_chars = np.load('playlists/%s/ancillary_char.npy'%genre)
+    vocab_size = len(text_to_int)
     X = np.load('playlists/%s/X_sl%d_char.npy'%(genre,seq_length))
     y = np.load('playlists/%s/y_sl%d_char.npy'%(genre,seq_length))
     
@@ -35,11 +45,12 @@ def train_model(genre, dir_model, seq_length, batch_size, epochs=100):
     except:
         print("generating new model")
         model = Sequential()
+        #model.add(Embedding(vocab_size, lstm_size, input_length=(batch_size, seq_length))
+        model.add(GRU(lstm_size, dropout=0.2, recurrent_dropout=0.2, return_sequences=True,
+                      input_shape=(seq_length, vocab_size)))
         
-        model.add(GRU(256, dropout=0.2, recurrent_dropout=0.2, return_sequences=True,
-                      input_shape=(X.shape[1], X.shape[2])))
         # output shape: (batch_size, seq_len, vocab_size)
-        model.add(TimeDistributed(Dense(y.shape[2], activation='softmax')))
+        model.add(TimeDistributed(Dense(vocab_size, activation='softmax')))
         loss = 'categorical_crossentropy'
 
         lr = 1e-3
@@ -51,18 +62,22 @@ def train_model(genre, dir_model, seq_length, batch_size, epochs=100):
     print(model.summary())
     checkpoint = ModelCheckpoint(dir_model, monitor='loss', verbose=1, save_best_only=True, mode='min')
     callbacks_list = [checkpoint]
-    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,
-              callbacks=callbacks_list, validation_data=(X_test, y_test), verbose=1)
+    model.fit_generator(one_hot_gen(X_train, y_train, vocab_size, seq_length, batch_size),
+                        steps_per_epoch=len(X_train)/batch_size, epochs=epochs, callbacks=callbacks_list,
+                        validation_data=one_hot_gen(X_test, y_test, vocab_size, seq_length, batch_size),
+                        validation_steps=len(X_test)/batch_size)
+#    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,
+#              callbacks=callbacks_list, validation_data=(X_test, y_test), verbose=1)
     model.save(dir_model)
 
 if __name__ == '__main__':
     genre = 'pop-rock-edm'
     seq_length = int(sys.argv[1])
-    batch_size = 256
+    batch_size = 64
     
-    dir_model = 'models/%s_sl%d_char.h5'%(genre,seq_length)
+    dir_model = 'models/%s_sl%d_char.h5'%(genre, seq_length)
     
-    train_model(genre,dir_model,seq_length,batch_size)
+    train_model(genre, dir_model, seq_length, batch_size)
 
 
 
