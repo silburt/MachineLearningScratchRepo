93f5de78dc316dc26a687585d5484408a731f2c3
silburt <silburt@astro.utoronto.ca>
2018-01-15 20:55:46 -0500

    edit

--- Lyric_analysis/plot_training.py
+++ Lyric_analysis/plot_training.py
@@ -6,7 +6,7 @@ import numpy as np
 seq_lengths = [25,50,75,100,125,150,175]
 for seq in seq_lengths:
     #file = 'output/sl%d_word.txt'%f
-    file = 'output/sl%d_character.txt'%f
+    file = 'output/sl%d_character.txt'%seq
 
     lines = open(file,'r').readlines()
     loss = []
